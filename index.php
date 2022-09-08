cc1fb11f0aa0a130e3e5419f93a8cc105d301e6e
syailendra <syailendrakusmayogi@gmail.com>
2022-09-09 01:26:05 +0700

    new upload

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE php>
+<php>
+    <head>
+        <meta charset="utf-8">
+        <title>Belajar Git - Project 01</title>
+    </head>
+    <body>
+        <p>Hello Gais</p>
+</body>
+</php>
